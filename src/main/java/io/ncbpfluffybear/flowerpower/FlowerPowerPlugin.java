2a417a17a3ba286057c1c91ba0fec50fed01d7fa
wickidcow <jaywago@gmail.com>
2026-08-30 11:53:42 -0400

    refactor: remove obsolete Blob Builds updater [ci skip]

--- src/main/java/io/ncbpfluffybear/flowerpower/FlowerPowerPlugin.java
+++ src/main/java/io/ncbpfluffybear/flowerpower/FlowerPowerPlugin.java
@@ -1,23 +1,17 @@
 package io.ncbpfluffybear.flowerpower;
 
-import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.BlobBuildUpdater;
-import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.GitHubBuildsUpdater;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.ncbpfluffybear.flowerpower.setup.FlowerPowerItemSetup;
 import io.ncbpfluffybear.flowerpower.setup.ResearchSetup;
-import org.bstats.bukkit.Metrics;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import listeners.Events;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.plugin.java.JavaPlugin;
 import utils.Utils;
 
 import javax.annotation.Nonnull;
-import java.lang.reflect.Field;
 
 /**
- * The main class of the FlowerPower addon
+ * The main class of the FlowerPower addon.
  *
  * @author NCBPFluffyBear
  */
@@ -27,37 +21,18 @@ public class FlowerPowerPlugin extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
-
         instance = this;
 
-        // bStats Metrics
-        final Metrics metrics = new Metrics(this, 12349);
+        new Metrics(this, 12349);
 
-        // Read something from your config.yml
-        Config cfg = new Config(this);
-
-        if (cfg.getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
-            new BlobBuildUpdater(this, getFile(), "FlowerPower", "Dev").start();
-        }
-
-        // Register events
         Utils.registerEvents(new Events());
-
-        // Register all items
-        FlowerPowerItemSetup.setup(getInstance());
-
-        // Register all researches
+        FlowerPowerItemSetup.setup(this);
         ResearchSetup.setup();
     }
 
     @Override
-    public void onDisable() {
-        // Logic for disabling the plugin...
-    }
-
-    @Override
     public String getBugTrackerURL() {
-        return "https://github.com/NCBPFluffyBear/FlowerPower/issues";
+        return "https://github.com/wickidcow/SF_FlowerPower/issues";
     }
 
     @Nonnull
@@ -69,5 +44,4 @@ public class FlowerPowerPlugin extends JavaPlugin implements SlimefunAddon {
     public static FlowerPowerPlugin getInstance() {
         return instance;
     }
-
 }
