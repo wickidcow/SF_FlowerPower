be787c3273836b5fc3199958d463d1bf5d971bf2
wickidcow <jaywago@gmail.com>
2026-08-30 11:58:59 -0400

    fix: update modern Paper attribute names

--- src/main/java/io/ncbpfluffybear/flowerpower/items/AttributeCharms.java
+++ src/main/java/io/ncbpfluffybear/flowerpower/items/AttributeCharms.java
@@ -15,7 +15,6 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -23,12 +22,10 @@ import utils.Utils;
 
 import javax.annotation.Nonnull;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Charms are held in the offhand to provide
- * various buffs to the player
+ * Charms are held in the offhand to provide various buffs to the player.
  *
  * @author NCBPFluffyBear
  */
@@ -39,7 +36,6 @@ public class AttributeCharms extends SimpleSlimefunItem<ItemUseHandler> implemen
 
     private final ItemSetting<Double> minLevel;
     private final ItemSetting<Double> maxLevel;
-
     private final Charm type;
 
     public AttributeCharms(ItemGroup category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe, Charm type) {
@@ -61,40 +57,32 @@ public class AttributeCharms extends SimpleSlimefunItem<ItemUseHandler> implemen
             ItemMeta charmMeta = charm.getItemMeta();
             Player p = e.getPlayer();
 
-            // Don't reinspect charms
             if (PersistentDataAPI.getByte(charmMeta, inspectedKey, (byte) 0) == 1) {
                 return;
             }
 
-            // Add specified attribute to offhand
             double level = ThreadLocalRandom.current().nextDouble(minLevel.getValue(), maxLevel.getValue());
             NamespacedKey key = type.attribute.getKey();
-            AttributeModifier modifier = new AttributeModifier(key,
-                    level, type.operation, EquipmentSlotGroup.OFFHAND);
+            AttributeModifier modifier = new AttributeModifier(key, level, type.operation, EquipmentSlotGroup.OFFHAND);
             charmMeta.addAttributeModifier(type.attribute, modifier);
 
-            // Update lore
             List<String> lore = charmMeta.getLore();
             lore.set(LORE_INDEX, Utils.color("&aThis charm has been inspected"));
             charmMeta.setLore(lore);
 
             p.playSound(p.getLocation(), Sound.BLOCK_BELL_RESONATE, 1, 1);
-
-            // Set inspected status to true
             PersistentDataAPI.setByte(charmMeta, inspectedKey, (byte) 1);
             charm.setItemMeta(charmMeta);
-
         };
     }
 
     public enum Charm {
-
-        MOVEMENT_SPEED(Attribute.GENERIC_MOVEMENT_SPEED, 0.01, 0.2, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
-        ATTACK_SPEED(Attribute.GENERIC_ATTACK_SPEED, 0.1, 0.5, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
-        FLY_SPEED(Attribute.GENERIC_FLYING_SPEED, 0.01, 1, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
-        DAMAGE(Attribute.GENERIC_ATTACK_DAMAGE, 0.01, 1, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
-        MAX_HEALTH(Attribute.GENERIC_MAX_HEALTH, 1, 5, AttributeModifier.Operation.ADD_NUMBER),
-        KNOCKBACK_RESISTANCE(Attribute.GENERIC_KNOCKBACK_RESISTANCE, 0.1, 0.5, AttributeModifier.Operation.ADD_NUMBER);
+        MOVEMENT_SPEED(Attribute.MOVEMENT_SPEED, 0.01, 0.2, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
+        ATTACK_SPEED(Attribute.ATTACK_SPEED, 0.1, 0.5, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
+        FLY_SPEED(Attribute.FLYING_SPEED, 0.01, 1, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
+        DAMAGE(Attribute.ATTACK_DAMAGE, 0.01, 1, AttributeModifier.Operation.MULTIPLY_SCALAR_1),
+        MAX_HEALTH(Attribute.MAX_HEALTH, 1, 5, AttributeModifier.Operation.ADD_NUMBER),
+        KNOCKBACK_RESISTANCE(Attribute.KNOCKBACK_RESISTANCE, 0.1, 0.5, AttributeModifier.Operation.ADD_NUMBER);
 
         private final Attribute attribute;
         private final double minLvl;
